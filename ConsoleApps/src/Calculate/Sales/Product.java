02b037a93f6ea61a9522451477a3a97cf05fc046
ki7mt <ki7mt@yahoo.com>
2017-12-29 22:54:30 -0700

    Add product sales classes

---
+++ ConsoleApps/src/Calculate/Sales/Product.java
@@ -0,0 +1,77 @@
+/* 
+ *  Author ......: Greg, Beam, KI7MT, <ki7mt@yahoo.com>
+ *  Copyright ...: Copyright (C) 2017 GPLv3
+ *  Level .......: Intermediate
+ *  Target ......: Java Console Application
+ *  Description..: Class for Products
+ *  
+ */
+package Calculate.Sales;
+
+public class Product {
+
+	// initialize variables
+	private int productID;
+	private double productCost;
+
+	// constructor
+	public Product(int productID, double productCost)
+	{
+		super();
+		this.productID = productID;
+		this.productCost = productCost;
+		setProductID(0);
+	}
+
+	/** @return the product ID */
+	public int getProductID()
+	{
+		return productID;
+	}
+	
+	/** @param productID sets the product number */
+	public void setProductID(int productID)
+	{
+		switch (productID)
+		{
+			case 1:
+				setProductCost(2.98);
+				break;
+		
+			case 2:
+				setProductCost(4.50);
+				break;
+
+			case 3:
+				setProductCost(9.98);
+				break;				
+
+			case 4:
+				setProductCost(4.49);
+				break;
+
+			case 5:
+				setProductCost(6.87);
+				break;	
+
+			default:
+				setProductCost(0.00);
+				break;
+		} // END - Case switch
+		
+		this.productID = productID;
+	}
+	
+	/** @return returns the product cost based on ID */
+	public double getProductCost()
+	{
+		return productCost;
+	}
+
+	/** @param productCost sets the product cost */
+	public void setProductCost(double productCost)
+	{
+		this.productCost = productCost;
+	}
+
+} /* END - Product class */
---
+++ ConsoleApps/src/Calculate/Sales/ProductDriver.java
@@ -0,0 +1,127 @@
+/* 
+ *  Author ......: Greg, Beam, KI7MT, <ki7mt@yahoo.com>
+ *  Copyright ...: Copyright (C) 2017 GPLv3
+ *  Level .......: Intermediate
+ *  Target ......: Java Console Application
+ *  Description..: Driver for Product Sales
+ *  
+ */
+package Calculate.Sales;
+
+import java.util.Scanner;
+
+
+public class ProductDriver
+{
+	public static void main(String[] args) {
+		
+		// initialize loop control and quantity variables.
+		int pcount;
+		int number = 0;
+		int p1Count = 0;
+		int p2Count = 0;
+		int p3Count = 0;
+		int p4Count = 0;
+		int p5Count = 0;
+		int quantity = 0;
+		double itemTotal;
+				
+		// initialize the Scanner.
+		Scanner input = new Scanner(System.in);
+		
+		// start while loop for product and quantity entry.
+		while (number != -1)
+		{
+			// user entry, product ID.
+			System.out.print("Enter Product ID (1-5) or (-1) to quit : ");
+			number = input.nextInt();
+
+			if (number != -1)
+			{
+				// user entry, product quantity.
+				System.out.print("Enter Quantity : ");
+				quantity = input.nextInt();
+							
+				if (number == 1)
+				{
+					p1Count += quantity;
+				}	
+				if (number == 2)
+				{
+					p2Count += quantity;
+				}
+				if (number == 3)
+				{
+					p3Count += quantity;
+				}
+				if (number == 4)
+				{
+					p4Count += quantity;
+				}
+				if (number == 5)
+				{
+					p5Count += quantity;
+				}
+			}
+			System.out.print("\n");
+		} // END - Entry loop.
+		
+		// close input scanner to prevent resource leaks
+		input.close();
+		System.out.println("** End of Product Entry **");
+				
+		// print summary header
+		System.out.print("\nItem       Qty\tCosts");
+		System.out.print("\n-------------------------");
+		
+		// create a Product Class instance
+		Product value = new Product(0,0.00);
+		
+		// loop through product number (1-5) and calculate the summaries
+		for (int count = 1; count <= 5; count++)
+		{
+			// set product ID which sets the ProductCost
+			value.setProductID(count);
+
+			switch (count)
+			{
+				case 1:
+					itemTotal = p1Count * value.getProductCost();
+					pcount = p1Count; 
+					break;
+			
+				case 2:
+					itemTotal = p2Count * value.getProductCost();;
+					pcount = p2Count;
+					break;
+
+				case 3:
+					itemTotal = p3Count * value.getProductCost();
+					pcount = p3Count;
+					break;				
+
+				case 4:
+					itemTotal = p4Count * value.getProductCost();
+					pcount = p4Count;
+					break;
+
+				case 5:
+					itemTotal = p5Count * value.getProductCost();
+					pcount = p5Count;
+					break;	
+
+				default:
+					itemTotal = 0.00;
+					pcount = 0;
+					break;
+			} // END - Case switch
+			
+			// print the product line totals
+			System.out.printf("\nProduct-%s   %s   $%.2f", count, pcount, itemTotal);
+
+		} // END - for loop
+		System.out.print("\n");
+
+	} // END - Main Method
+
+} /* END - ProductDriver class */
