8d570afd2bf82b540d5493536595c251377d56fe
ki7mt <ki7mt@yahoo.com>
2017-12-29 23:34:10 -0700

    Add animal sighting code and update comments

---
+++ ConsoleApps/src/beam/example/animal/sighting/Animal.java
@@ -0,0 +1,85 @@
+/* 
+ *  Author ......: Greg, Beam, KI7MT, <ki7mt@yahoo.com>
+ *  Copyright ...: Copyright (C) 2017 GPLv3
+ *  Level .......: Intermediate
+ *  Target ......: Java Console Application
+ *  Description..: Class for Animal Sighting
+ *  
+ */
+package beam.example.animal.sighting;
+
+public class Animal
+{
+	private String animalName;
+	private int animalCount;
+
+	// constructor: super, all fields
+	public Animal(String animalName, int animalCount) {
+		super();
+		this.animalName = animalName;
+		this.animalCount = animalCount;
+	}
+
+	// constructor: name only
+	public String getAnimalName()
+	{
+		return animalName;
+	}
+
+	/* GETTERS and SETTERS */
+	public Animal(String animalName) {
+		super();
+		this.animalName = animalName;
+	}
+	public void setAnimalName(String animalName)
+	{
+		this.animalName = animalName;
+	}
+	public int getAnimalCount()
+	{
+		return animalCount;
+	}
+	public void setAnimalCount(int animalCount)
+	{
+		this.animalCount = animalCount;
+	}
+	
+	// override for hashCode method
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((animalName == null) ? 0 : animalName.hashCode());
+		return result;
+	}
+	
+	// override for equals method
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Animal other = (Animal) obj;
+		if (animalName == null)
+		{
+			if (other.animalName != null)
+				return false;
+		} else if (!animalName.equals(other.animalName))
+			return false;
+		return true;
+	}
+	
+	// object toString method
+	@Override
+	public String toString()
+	{
+		return "Animal Type: " + animalName + " Count: " + animalCount;
+	}
+
+} /* END - Animal Class */
---
+++ ConsoleApps/src/beam/example/animal/sighting/AnimalDriver.java
@@ -0,0 +1,93 @@
+/* 
+ *  Author ......: Greg, Beam, KI7MT, <ki7mt@yahoo.com>
+ *  Copyright ...: Copyright (C) 2017 GPLv3
+ *  Level .......: Intermediate
+ *  Target ......: Java Console Application
+ *  Description..: Class for Animal Sighting Driver
+ *  
+ */
+package beam.example.animal.sighting;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class AnimalDriver
+{
+	public static void main(String[] args)
+	{
+		// setup the array list
+		ArrayList<Animal> sighted = new ArrayList<Animal>();
+
+		// start the scanner
+		Scanner input = new Scanner(System.in);
+		
+		// set process variables
+		String inputAnimal;
+		boolean doneSightSeeing = false;
+
+		System.out.println("** Prickly Pear Creek Safari Sight Seeing **");
+		System.out.println("--------------------------------------------");
+		System.out.println("Enter the animal you see, or quit to exit.");
+		System.out.println("");
+		
+		// start sight seeing loop :-)
+		while (!doneSightSeeing)
+		{
+			System.out.print("Enter Animal : ");
+			inputAnimal = input.next().toLowerCase();
+			doneSightSeeing = tooManyLions(inputAnimal);
+			
+			// if we're not scared of the Lions, or tired, keep going
+			if (!doneSightSeeing)
+			{
+				Animal animal = new Animal(inputAnimal);
+
+				if (!sighted.contains(animal))
+				{
+					sighted.add(animal);
+					animal.setAnimalCount(+ 1);
+				}
+				else
+				{
+					Animal dupeSighting = sighted.get(sighted.indexOf(animal));
+					dupeSighting.setAnimalCount(dupeSighting.getAnimalCount() + 1);
+				}
+			}
+		} // end while loop
+		
+		
+		// loop through the arrayList and print what was observed
+		System.out.println("");
+		System.out.println("Summary of Animals Identified");
+		System.out.println("------------------------------------------");
+
+		for (Animal animals : sighted)
+		{
+			System.out.println(animals);
+		}
+			
+		// close the scanner
+		input.close();
+		
+		// just more screen candy
+		System.out.println("");
+		System.out.println("Thank you for using Prickly Pear Creek Safari");
+	}
+
+	// get me out of here method
+	public static boolean tooManyLions(String input)
+	{
+		String check;
+		check = input.toUpperCase();
+
+		if (check.equals("QUIT"))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+} /* END - Animal Driver */
