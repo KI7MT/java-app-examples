017677cc4507be34812cadfd123de2c02827c7b2
ki7mt <ki7mt@yahoo.com>
2017-12-29 22:51:08 -0700

    Add parking garage classes

---
+++ ConsoleApps/src/Parking/Garage/ParkingCharge.java
@@ -0,0 +1,82 @@
+/* 
+ *  Author ......: Greg, Beam, KI7MT, <ki7mt@yahoo.com>
+ *  Copyright ...: Copyright (C) 2017 GPLv3
+ *  Level .......: Intermediate
+ *  Target ......: Java Console Application
+ *  Description..: Class for Parking Charges
+ *  
+ */
+package Parking.Garage;
+
+public class ParkingCharge
+{
+
+    // set final variables
+	private static final double MINCHAEGE = 2.0;
+    private static final double MAXCHARGE = 10.0;
+    private static final double INCCHARGE = 0.50;
+
+    // Set min and max hours
+    private static final double MINHOURS = 3.0;
+    private static final double MAXHOURS = 24;
+
+    // Set variable to keeping track of daily charges
+    private double runTotal;
+
+    // constructor
+    public ParkingCharge(double runTotal)
+    {
+        // Assign instance variables.
+        this.runTotal = runTotal;
+
+    }
+
+    /** @return runTotal returns running total of charges */
+    public double getRunTotal()
+    {
+    	return runTotal;
+    }
+
+    /** @param runTotal sets running total of charges */
+    public void setRunTotal(double runTotal)
+    {
+        this.runTotal = getRunTotal() + runTotal;
+    }
+
+    /** Calculate Charges - calculate customer and total running charges */
+    public static double calculateCharges(double customerHours)
+    {
+        // Set variable for customer hours
+        double customerCharge = 0.0;
+
+       // If customerHours are < 3 first, set MINCHAEGE
+        if (customerHours <= MINHOURS)
+        {
+            customerCharge = MINCHAEGE;
+        }
+
+        // If customerHours greater than MINHOURS and less than MAXHOURS
+        // calculate the charge
+        if((customerHours > MINHOURS) && (customerHours < MAXHOURS))
+        {
+            customerCharge = INCCHARGE * (Math.ceil(customerHours) - MINHOURS)
+                    + MINCHAEGE;
+
+            if (customerCharge > MAXCHARGE)
+            {
+                customerCharge = MAXCHARGE;
+            }
+        }
+
+        // If customerHours are greater than MAXHOURS, set MAXCHARGE
+        if(customerHours > MAXHOURS)
+        {
+            customerCharge = MAXCHARGE;
+        }
+
+        // Return customer parking charge
+        return customerCharge;
+
+    } // END - Calculate Charges method.
+
+} /* END - ParkingCharge class */
---
+++ ConsoleApps/src/Parking/Garage/ParkingChargeDriver.java
@@ -0,0 +1,56 @@
+/* 
+ *  Author ......: Greg, Beam, KI7MT, <ki7mt@yahoo.com>
+ *  Copyright ...: Copyright (C) 2017 GPLv3
+ *  Level .......: Intermediate
+ *  Target ......: Java Console Application
+ *  Description..: Driver for Parking Garage
+ *  
+ */
+package Parking.Garage;
+
+import java.util.Scanner;
+
+public class ParkingChargeDriver
+{
+
+	public static void main(String[] args)
+	{
+		// Initialize the Scanner
+		Scanner input = new Scanner(System.in);
+
+		// Initialize loop control and counter variables.
+		double hours = 0.0;
+		double charges;
+
+		// Set the input objects to 0 initially.
+		ParkingCharge costs = new ParkingCharge(0.0);
+
+		// Start while loop for customer hour entry.
+		while (hours != -1)
+		{
+            System.out.printf("Enter Hours (-1 to quit)..: ");
+            hours = input.nextDouble();
+
+            // Only allow entry if hours are greater than zero
+			if(hours > 0)
+			{
+                charges = ParkingCharge.calculateCharges(hours);
+                costs.setRunTotal(charges);
+                System.out.printf("Customer charge is........: " +
+                        "$%.2f ", charges);
+                System.out.print("\n");
+                System.out.printf("Running Daily Total.......: " +
+                        "$%.2f%n", costs.getRunTotal());
+            }
+            System.out.print("\n");
+		} // END - Customer Hours Entry.
+
+		// Close the input scanner ( prevent resource leaks )
+		input.close();
+
+        // END - Customer Hour Entry.
+        System.out.printf("Total Daily Reciepts = $%.2f%n", costs.getRunTotal());
+	
+	} // END - Main Method.
+	
+} /* END - ParkingChangeDriver class */
